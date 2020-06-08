22af04a2a4605aaabca5517ffa2c7ebd02224f4b
Illia <kriminalmykriminal@gmail.com>
2020-06-08 17:34:01 +0300

    Structure of the project is created

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export {default as Container} from './Container/index'
+
+//export {default as Header} from './Header/index'
+//export {default as AcquaintedBlock} from './AcquaintedBlock/index'
+//export {default as Button} from './Button/index'
+//export {default as Footer} from './Footer/index'
+//export {default as Logo} from './Logo/index'
+//export {default as Navigation} from './Navigation/index'
+//export {default as PreviewBlock} from './PreviewBlock/index'
+//export {default as User} from './User/index'
+//export {default as UsersBlock} from './UsersBlock/index'
---
+++ src/modules/index.js
@@ -0,0 +1,2 @@
+//export {default as UsersList} from './UsersList/index'
+//export {default as RegisterForm} from './RegisterForm/index'
