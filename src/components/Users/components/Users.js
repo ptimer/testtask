b51d2b02660d75a1965c629a9f271e11c63f76bd
Illia <kriminalmykriminal@gmail.com>
2020-06-13 11:19:31 +0300

    users fetched

--- src/components/Users/components/Users.js
+++ src/components/Users/components/Users.js
@@ -1,24 +1,37 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
+import { Button } from '../../UI'
 import UsersList from './List';
 import UsersUser from './User';
 
 import './Users.scss'
 
 
-export default ({showMore, showButton, items, posted}) => {
+export default function Users(props){
 
 	return (
 			<div className="Users">
+					<h2 class="Users__title">Let's get acquainted</h2>
+					<p class="Users__description">Attention! Sorting users by registration date</p>
+
 					<UsersList>
-						{/*
-							items.map(item => <UsersUser key={item.id} user={{img: item.photo,
-																	name: item.name, 
-																	phone: item.phone, 
-																	position: item.position, 
-																	email: item.email}} />)
-						*/}
+						{
+							props.users.map(item => <UsersUser key={item.id} user={{img: item.photo,
+																							name: item.name, 
+																							phone: item.phone, 
+																							position: item.position, 
+																							email: item.email}} />)
+						}
 					</UsersList>
+					
+					<div className="Users__button">
+						<Button text="Show more" />
+					</div>
 			</div>
 	)
+}
+
+Users.propTypes = {
+	users: propTypes.array
 }
--- src/components/Users/containers/Users.js
+++ src/components/Users/containers/Users.js
@@ -1,2 +1,29 @@
-import Users from '../components/Users';
-export default Users;
+import React, {useState, useEffect} from 'react'
+
+import Users from '../components/Users'
+
+export default props => {
+	const [users, setUsers] = useState([])
+
+	useEffect(_ => {
+		Utils.getUsers({count: 6}).then(d => setUsers(d.users));
+	}, [])
+
+	return (
+		<Users users={users} />
+	)
+}
+
+const Utils = {
+	getUsers: params => {
+		return fetch(`https://frontend-test-assignment-api.abz.agency/api/v1/users?page=1&count=${params.count}`)
+		.then(r => r.json())
+		.then(d => {
+			if(d.success){
+				return d;
+			}else{
+				throw "Не получилось получить список пользователей";
+			}
+		})
+	}
+}
