1448645c4f446072a54673e211e136eb3c8665bc
Illia <kriminalmykriminal@gmail.com>
2020-06-17 10:52:49 +0300

    modal window is ready

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,20 +1,42 @@
 import React, {Fragment} from 'react'
 import propTypes from 'prop-types'
 import './Modal.scss'
+import Modal from 'react-modal'
 import {Button} from '../'
+import closeIcon from '../../assets/img/close.svg'
 
-const Modal = ({visible, close}) => (
-	<Fragment>
-		{visible && <div className="modal">
-				<Button onClick={close}>Great</Button>
-			</div>
-		}
-	</Fragment>
+const ModalComponent = ({visible, close}) => (
+		<div>
+	        <Modal
+	          isOpen={visible}
+	          onRequestClose={close}
+	          className={'modal'}
+	          contentLabel="Example Modal"
+	          overlayClassName="Overlay"
+	        >
+				<div className='modal__top'>
+					<div className='modal__title'>
+						<span>Congratulations</span>
+					</div>
+					<div className='modal__close'>
+						<img src={closeIcon} alt="" onClick={close}/>
+					</div>
+				</div>
+
+				<div className='modal__center'>
+					<span>You have successfully passed the registration</span>
+				</div>
+
+				<div className='modal__bottom'>
+					<Button className={'modal__close_button'} onClick={close}>Great</Button>
+				</div>
+	        </Modal>
+	    </div>
 )
 
-Modal.propTypes = {
+ModalComponent.propTypes = {
 	visible: propTypes.bool,
 	close: propTypes.func,
 }
 
-export default Modal
+export default ModalComponent
