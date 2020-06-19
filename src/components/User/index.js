ff420687d33f0f6173497e94897115b5535dd452
Illia <kriminalmykriminal@gmail.com>
2020-06-19 15:58:53 +0300

    pixel perfect header, banner, about me

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -9,7 +9,10 @@ export default ({user, className}) => (
 		{user && <Fragment><img src={!user.photo ? photoCoverSvg : user.photo} alt={user.name}/>
 				 <h3 className='user_name'>{user.name}</h3>
 				 <span className='user__position'>{user.position}</span>
-				 <span className='user__email'>{user.email}</span>
+				 <span className={classNames('user__email', {'user__tooltip': user.email.length > 20})}>
+				 	{user.email.length > 20 ? `${user.email.substring(0, 20)}...` : user.email}
+					{user.email.length > 20 ? <span className='user__tooltip_text'>{user.email}</span> : null}
+				 </span>
 				 <span className='user__phone'>{user.phone}</span></Fragment>
 	}
 	</div>
